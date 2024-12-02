7e1a78e25e0359cdd34ac7c8eee2c0f5cda38ecf
Александр Елисеев <a.eliseev@demiusgroup.ru>
2024-12-02 13:45:37 +0300

    changes

---
+++ index.js
@@ -0,0 +1,15 @@
+await ymaps3.ready;
+
+const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+
+const map = new YMap(
+    document.getElementById('app'),
+    {
+        location: {
+            center: [37.588144, 55.733842],
+            zoom: 10
+        }
+    }
+);
+
+map.addChild(new YMapDefaultSchemeLayer());
